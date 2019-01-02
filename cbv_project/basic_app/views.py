b4f6099535e0bcf0e6d2190355c547e8cc4607a6
storres8 <stevetorres7562@gmail.com>
2019-01-02 14:30:44 -0500

    set up listview and detailview for school in the views file.

--- cbv_project/basic_app/views.py
+++ cbv_project/basic_app/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.http import HttpResponse
+from basic_app.models import School, Student
 
 
 class CBView(View):
     def get(self,request):
         return HttpResponse("CBV!")
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['injectme'] = 'Basic injection!'
+#         return context
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'Basic injection!'
-        return context
+
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
