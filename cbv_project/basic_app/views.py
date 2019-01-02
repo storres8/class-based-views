3e9de9ce4b1c07af13d2ad3f9d5e289a823ca138
storres8 <stevetorres7562@gmail.com>
2019-01-02 12:10:02 -0500

    created view TemplateView.

--- cbv_project/basic_app/views.py
+++ cbv_project/basic_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 
 class CBView(View):
     def get(self,request):
         return HttpResponse("CBV!")
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'Basic injection!'
+        return context
